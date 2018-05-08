432e2ece9378e2fa2f37fb7f5c30ba6b3b3743ed
Marc Sances <marcsances@gmail.com>
2018-05-08 20:41:14 +0200

    minor fix of the icons

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createWindow() {
     height: 600,
     resizable: true,
     titleBarStyle: 'hidden',
-    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+    icon: path.join(__dirname, 'assets/icons/64x64.png')
   });
   mainWindow.setMenu(null);
   mainWindow.loadURL("https://www.twitch.tv/");

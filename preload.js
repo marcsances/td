f061d1ea2353975e3a5dbb4acc3c6f85a9f70799
Marc Sances <marcsances@gmail.com>
2018-05-14 12:10:52 +0200

    implemented logger proxy, fixes #17

--- preload.js
+++ preload.js
@@ -1,3 +1,7 @@
 var logger = require('electron-log');
 
-window.console = logger;
+window.console.log = logger.log;
+window.console.warn = logger.warn;
+window.console.info = logger.info;
+window.console.error = logger.error;
+window.console.debug = logger.debug;
